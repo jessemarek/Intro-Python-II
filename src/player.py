84768bdc41ce94841b13d95f148c3c71e5a1604b
unknown <dethgrenade@gmail.com>
2020-07-08 12:26:42 -0700

    initial commit. planning out the Room and Player classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# Player has a name and and current room
+# Player starts in the "outside" Room
+# Player will need a list of items they can carry
+
+class Player:
+    def __init__(self, name, current_room="outside"):
+        self.name = name
+        self.current_room = current_room
--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# Room has a name and description
+# Room will need paths to other rooms to the N, E, S and W
+# Room will need to be able to store items
+# Items may be either initially in the room or dropped by the player
+
+class Room:
+    def __inti__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = "none"
+        self.e_to = "none"
+        self.s_to = "none"
+        self.w_to = "none"
